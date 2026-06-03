ff751d784e9e98820dd9272cd7fb601df6f0990c
s4teen <112770218+s4teen@users.noreply.github.com>
2026-06-03 06:56:51 +0300

    Create Constants.h

---
+++ src./Core/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SFML/System/Vector2.hpp>
+
+namespace Constants
+{
+    inline constexpr sf::Vector2f DesignSize{1800.f, 1015.f};
+    inline constexpr const char* WindowTitle = "Cuc Drag Racing";
+}
